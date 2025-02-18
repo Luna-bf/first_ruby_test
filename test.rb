f839184826713681dbc9bce9c64c9c38abd2102b
Sunnyy05 <lburattifayolle@gmail.com>
2025-02-18 16:07:23 +0100

    Added a third 'Hello World'

--- test.rb
+++ test.rb
@@ -2,6 +2,8 @@ puts "Hello World"
 
 print "Hello World" #C'est mieux d'utiliser 'puts' car il ajoute un saut de ligne, ce que 'print' ne fait pas.
 
+p "Hello World" #'p', quant à lui, ajoute des guillemets. Il vaudrait mieux ce concentrer sur 'puts' pour le moment.
+
 name = "Diane"
 
 if name == "Tim"
