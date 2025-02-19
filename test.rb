8813b197bb8d5cdb0939e10b029ec252597c9aa7
Sunnyy05 <lburattifayolle@gmail.com>
2025-02-19 13:16:20 +0100

    Understanding data types in Ruby (int and float added)

--- test.rb
+++ test.rb
@@ -14,14 +14,22 @@ multiline comment.
 # Type de données
 
 =begin
-En Ruby, tout est un objet appartenant à une classe. On peut créer une classe comme ceci : object.class.
+En Ruby, tout est un objet appartenant à une classe. On peut créer une classe comme ceci : object.class
 Voici un exemple plus précis :
 =end
 
-puts "Hello".class # Cela va me donner une chaîne de caractères (String) 
+puts "Hello".class # Cela va me donner une chaîne de caractères (String).
+
+# On peut faire pareil avec les autres types de données, comme les nombres entiers, décimaux, etc.
+
+puts 19.class # Nombre entier
+
+puts 1_000_000.class # Les tirets du 8 aident simplement à rendre les grands nombres lisibles.
+
+puts 5.85.class # Attention à bien mettre le '.' et pas la ',' sinon le nombre sera considéré comme entier.
 
 
-# Simple Ruby lines from "How to set up VS Code for Ruby", made before everything that's above
+# Simple Ruby lines from "How to set up VS Code for Ruby", made before everything that's above.
 
 name = "Diane"
 
